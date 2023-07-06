90993050d74e14ba1dbbc2c6eaa5313e60e1a28a
vivekananda-16 <95311184+vivekananda-16@users.noreply.github.com>
2023-07-06 23:14:09 +0530

    First Commit
    First Date : 06-07-2023

---
+++ cart.js
@@ -0,0 +1,18 @@
+// Create an empty cart array
+const cart = [];
+
+// Example books to add to the cart
+const book1 = { title: "DEATH COMES FOR THE ARCHBISHOP", author: "WILLA CATHER" };
+const book2 = { title: "NORMAL PEOPLE", author: "SALLY ROONEY" };
+const book3 = { title: "MASTER SLAVE HUSBAND WIFE", author: "ILYON WOO" };
+const book4 = { title: "WRONG PLACE WRONG TIME", author: "GILLIAN McALLISTER" };
+// Add books to the cart
+cart.push(book1);
+cart.push(book2);
+cart.push(book3);
+cart.push(book4);
+
+// Display the contents of the cart
+console.log(cart);
+
+
